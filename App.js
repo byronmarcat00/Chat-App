8fd62b1b7d4956fe8e8b23b2ea0d31a120b676e1
Byron Marcatoma <byronmarcatoma00@gmail.com>
2023-07-24 15:02:20 -0400

    Authentication

--- App.js
+++ App.js
@@ -1,11 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import SignIn from './src/components/SignIn';
+import { Auth } from 'aws-amplify';
+import { Amplify } from 'aws-amplify';
+import awsconfig from './src/aws-exports';
+Amplify.configure(awsconfig);
 
 export default function App() {
+
+  const signOut= async() =>{
+
+    try {
+      await Auth.signout();
+  
+    } catch (e) {
+      console.log("Error loging out", e);
+    }
+
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <SignIn />
     </View>
   );
 }
---
+++ src/components/SignIn.js
@@ -0,0 +1,18 @@
+import * as React from "react";
+import MyInput  from "./MyInput";
+import MyButton from "./MyButton";
+import MyText from "./MyText";
+import {Button} from "react-native";
+
+export default function SignIn(){
+
+    return (
+        <React.Fragment>
+            <MyText type="title"> Sign In</MyText>
+            <MyInput label={"Email"} />
+            <MyInput label ={"Password"} secureTextEntry />
+            <MyButton title={'Sign In'} />
+            <Button title="Sign Up" />
+        </React.Fragment>
+    )
+}
