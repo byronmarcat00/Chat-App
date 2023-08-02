f37b2c3f5a2d4f9a71b14b81ae59610a311dff9c
Byron Marcatoma <byronmarcatoma00@gmail.com>
2023-08-02 11:01:48 -0400

    onbaording-Screen

--- src/navigation/Root.js
+++ src/navigation/Root.js
@@ -5,6 +5,9 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 import Home from "../screens/Home";
 import Profile from "../screens/Profile";
+import Onboarding from "../screens/Onboarding";
+import {Ionicons} from "@expo/vector-icons"
+import Chats from "../screens/Chats";
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -14,10 +17,69 @@ export default function Root({colorScheme}) {
     <NavigationContainer 
     theme={colorScheme === 'dark' ? DarkTheme: DefaultTheme}
     >
-      <Tab.Navigator initialRouteName="Home">
-        <Tab.Screen name="Home" component={Home} />
-        <Tab.Screen name="Profile" component={Profile}/>
-      </Tab.Navigator>
+      <BottomNavigation />
+      
     </NavigationContainer>
   );
+}
+
+function BottomNavigation(){
+
+  return (
+    <Tab.Navigator initialRouteName="Home">
+    <Tab.Screen name="HomeStack" component={HomeStack}  
+    options={{
+      tabBarIcon:({color}) => (<TabBarIcon name="ios-home" color={color} />),
+      headerShown:false,
+      tabBarLabel:"Home",
+    
+    }}
+
+    />
+
+<Tab.Screen name="chats" component={Chats}
+    options={{
+      tabBarIcon:({color}) => (<TabBarIcon name="md-chatbubbles" color={color} />),
+     
+    
+    }}/>
+
+
+    <Tab.Screen name="Profile" component={Profile}
+    options={{
+      tabBarIcon:({color}) => (<TabBarIcon name="person-circle" color={color} />),
+      headerShown:false,
+      tabBarLabel:"Settings",
+    
+    }}/>
+  </Tab.Navigator>
+  )
+}
+
+function HomeStack(){
+
+  return ( 
+
+    <Stack.Navigator>
+
+      <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name = "Onboarding" component = {Onboarding}
+      options={{
+        headerShown:false,
+        presentation:"fullScreenModal",
+
+      }}
+      />
+
+    </Stack.Navigator>
+
+  )
+}
+
+
+
+function TabBarIcon(props) {
+
+  return <Ionicons size={28} style={{marginBottom:-3}} {...props}/>
+
 }
